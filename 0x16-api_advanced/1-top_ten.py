fdac7252957b6b9ee5603b1ddf1938859a4c7517
Richard Miruka Onsare <richardonsare15@gmail.com>
2024-01-16 23:08:55 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""
+querries Reddit API and print  the titles of the first 10 posts
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            'User-Agent': 'Mozilla/5.0'
     }
     params = {
-        "limit": 10
+            'limit': 10
     }
-    response = requests.get(url, headers=headers, params=params,
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
                             allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
+    if response.status_code != 200:
+        print(None)
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    res = response.json()
+    hot_posts = res['data']['children']
+    if len(hot_posts) == 0:
+        print(None)
+    else:
+        for post in hot_posts:
+            print(post['data']['title'])
